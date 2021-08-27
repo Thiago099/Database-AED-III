81ce39a56980daa4ec252e7b9437d71b2b1aec3c
Thiago <thiago.kaique01@gmail.com>
2021-08-27 18:32:01 -0300

    [refactor] deprecated todo

--- database/Table.java
+++ database/Table.java
@@ -142,7 +142,6 @@ public class Table<T extends Identified>
 
     public void update(T object)
     {
-        // TODO optimize: deleting and re-adding the same item from the index list
         delete(object.getId());
         write(object);
     }
