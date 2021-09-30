9d23c81e03fd34ebdda5d36de54bf036288ff978
Thiago <thiago.kaique01@gmail.com>
2021-09-30 20:37:20 -0300

    [feature] command line parser skeleton

---
+++ console/Main.java
@@ -0,0 +1,23 @@
+package console;
+import java.util.regex.*;
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        String input = String.join(" ", args);
+        Pattern p = Pattern.compile("--(\\w+)?<(.*?)?>");
+        Matcher m = p.matcher("--insert<a,b,c> --delete<a>");
+        while(m.find())
+        {
+            String command = m.group(1);
+            String[] parameters = m.group(2).split(",");
+            System.out.println(command);
+            for(String i: parameters)
+            {
+                System.out.println(i);
+            }
+            System.out.println();
+        }
+        
+    }
+}
